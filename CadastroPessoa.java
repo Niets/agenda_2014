9bf28a0635f0e9e96efaf391d719a7a8b253c946
alunos <alunos@ubuntu.ubuntu-domain>
2014-04-01 11:25:43 -0300

    Criação do CadastroPessoa

---
+++ CadastroPessoa.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class CadastroPessoa {
+
+    public static void main(String[] args){
+        
+        //Iniciando leitor
+        InputStream entradaSistema = System.in;
+        InputStreamReader leitor = new InputStreamReader(entradaSistema);
+        BufferedReader leitorEntrada = new BufferedReader(leitor);
+        String entradaTeclado = leitorEntrada.readLine();
+        
+        //Criação dos objetos do sistema
+        ControlePessoa umControle = new ControlePessoa();
+        Pessoa umaPessoa = new Pessoa();
+        
+        //Interação programa-usuãrio
+        System.out.println("Digite o nome da Pessoa:");
+        entradaTeclado = leitorEntrada.readLine();
+        String umNome = entradaTeclado;
+        umaPessoa.setNome(umNome);
+        
+        System.out.println("Digite o telefone da Pessoa:");
+        entradaTeclado = leitorEntrada.readLine();
+        String umTelefone = entradaTeclado;
+        umaPessoa.setTelefone(umTelefone);      
+        
+        umControle.adicionar(umaPessoa);
+    
+    }
+
+
+}
