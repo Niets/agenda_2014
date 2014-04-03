59e42aefeb15ddcf2e5e8ebecfcf891c2c34e3ac
Nicolas Boarin <nicolas.boarin@gmail.com>
2014-04-03 16:28:29 -0300

    Novos métodos para instanciar Pessoa.

--- CadastroPessoa.java
+++ CadastroPessoa.java
@@ -25,8 +25,43 @@ public class CadastroPessoa {
         String umTelefone = entradaTeclado;
         umaPessoa.setTelefone(umTelefone);    
         
-        umControle.adicionar(umaPessoa);
+        System.out.println("Digite a idade da Pessoa");
+        entradaTeclado = leitorEntrada.readLine();
+        int umaIdade = entradaTeclado;
+        umaPessoa.setIdade(umaIdade);
         
+        System.out.println("Digite o sexo da Pessoa (Válido apenas M ou F)");
+        entradaTeclado = leitorEntrada.readLine();
+        char umSexo = entradaTeclado;
+        umaPessoa.setSexo(umSexo);
+        
+        System.out.println("Digite o email da Pessoa:");
+        entradaTeclado = leitorEntrada.readLine();
+        String umEmail = entradaTeclado;
+        umaPessoa.setEmail(umEmail);
+        
+        System.out.println("Digite o endereço da Pessoa:");
+        entradaTeclado = leitorEntrada.readLine();
+        String umEndereco = entradaTeclado;
+        umaPessoa.setEndereco(umEndereco);
+        
+        System.out.println("Digite o RG da Pessoa:");
+        entradaTeclado = leitorEntrada.readLine();
+        String umRg = entradaTeclado;
+        umaPessoa.setRg(umRg);
+        
+        System.out.println("Digite o cpf da Pessoa:");
+        entradaTeclado = leitorEntrada.readLine();
+        String umCpf = entradaTeclado;
+        umaPessoa.setCpf(umCpf);
+        
+        
+        String mensagem = umControle.adicionar(umaPessoa);
+        
+         //conferindo saída
+        System.out.println("=================================");
+        System.out.println(mensagem);
+        System.out.println("=)");
     
     }
 
