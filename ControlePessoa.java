5344a0ced8639ded716b7b24832f69ec2703d7ed
Nicolas Boarin <nicolas.boarin@gmail.com>
2014-04-03 16:37:46 -0300

    Melhoria da classe com mais métodos

--- ControlePessoa.java
+++ ControlePessoa.java
@@ -13,20 +13,39 @@ public class ControlePessoa{
 		return listaPessoas; 
 	}
 	
-	public void adicionar(String umNome) {
-	    Pessoa umaPessoa = new Pessoa(umNome);
-	    listaPessoas.add(umaPessoa);
+	public adicionar(String umNome) {
+		String mensagem = "Pessoa adicionada com Sucesso!";
+		Pessoa umaPessoa = new Pessoa(umNome);
+		listaPessoas.add(umaPessoa);
+		return mensagem;
 	}
 	
-	public void adicionar(String umNome, String umTelefone){
-	    Pessoa umaPessoa = new Pessoa(umNome, umTelefone);
-	    listaPessoas.add(umaPessoa);
+	public adicionar(String umNome, String umTelefone){
+		String mensagem = "Pessoa adicionada com Sucesso!";
+		Pessoa umaPessoa = new Pessoa(umNome, umTelefone);
+		listaPessoas.add(umaPessoa);
+		return mensagem;
 	}
-
-    public void adicionar(Pessoa umaPessoa){
-        listaPessoas.add(umaPessoa);
-    }
 	
+	public adicionar(Pessoa umaPessoa){
+		String mensagem = "Pessoa adicionada com Sucesso!";
+		listaPessoas.add(umaPessoa);
+		return mensagem;
+	}
+	
+	public remover(Pessoa umPessoa) {
+	    	String mensagem = "Pessoa removida com Sucesso!";
+	        listaPessoas.remove(umPessoa);
+	        return mensagem;
+        }
+	
+	public Pessoa pesquisar(String nome) {
+	        for (Pessoa pessoa: listaPessoas) {
+	            if (pessoa.getNome().equalsIgnoreCase(nome))
+	            	return pessoa;
+        	}
+        	return null;
+    	}
 	
 	
 }
